9fbe513fe0954793f70540e919f2d90bdb3f1c3a
Duygu Gözen <nazduyguakyol@gmail.com>
2020-05-19 22:36:44 +0200

    transmittin data between sites

--- public/assests/scripts/scriptTwoPlayer.js
+++ public/assests/scripts/scriptTwoPlayer.js
@@ -1,12 +1,25 @@
+const socket = io();
+
+//This is the quote coming from the server
+socket.on('message', (message) => {
+	printToScreen(message);
+});
+
 const getText = (item) => {
 	const textContent = item.textContent;
-	console.log(textContent);
+	//Emit the quote to the server
+	socket.emit('quote', textContent);
 };
 
 let quotes = document.querySelectorAll('li');
-
 quotes.forEach((item) =>
 	item.addEventListener('click', function() {
 		getText(item);
 	})
 );
+
+//This is the function to output to the DOM
+function printToScreen(quote) {
+	const printScreen = document.getElementById('printScreen');
+	printScreen.innerHTML = quote;
+}
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//run when a client connects
+io.on('connection', (socket) => {
+	//All the emits have to be done here not outside...
+	//All the connections are written within this
+
+	console.log('new user online');
+	socket.emit('message', 'Are you ready to quote?');
+
+	//Broadcast when someone connects
+	//Broadcast emit will send to all besides the one who is connecting
+	socket.broadcast.emit('message', 'A quoter has joined online!');
+
+	//Runs when a some disconnects
+	socket.on('disconnect', () => {
+		io.emit('message', 'A quoter has left');
+	});
+
+	//Listen for the clicked quote
+	socket.on('quote', (quote) => {
+		console.log(quote);
+		io.emit('message', quote);
+	});
+});
 
 //if there is a enviroment variable with a port
 const PORT = 3000 || process.env.PORT;
 
 //run a server with port number
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
